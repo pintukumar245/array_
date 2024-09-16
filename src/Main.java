1f1a3a4d8f1f220b5c27944ca07996a1347f4b7a
pintu kumar <pintuk33621@gmail.com>
2024-09-16 08:41:13 +0530

    array of element:

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+         int [] age= new int[3];
+          age[0]=3;
+          age[1]=5;
+          age[2]=8;
+         System.out.println(age[0]);
+         System.out.println(age[1]);
+         System.out.println(age[2]);
+     }
+}
